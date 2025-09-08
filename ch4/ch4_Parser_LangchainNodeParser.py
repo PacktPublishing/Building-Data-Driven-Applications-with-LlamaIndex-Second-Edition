51c42d630bda837b662b1e85fb2f72f5cda6566b
AG-ITAcademy <andrei.gheorghiu@itacademy.ro>
2025-09-08 13:33:56 +0300

    change in langchain parser example

--- ch4/ch4_Parser_LangchainNodeParser.py
+++ ch4/ch4_Parser_LangchainNodeParser.py
@@ -1,13 +1,12 @@
 from llama_index.core.node_parser import LangchainNodeParser
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import NLTKTextSplitter
 from llama_index.readers.file import FlatReader
 from pathlib import Path
 
 reader = FlatReader()
 document = reader.load_data(Path("files/sample_document1.txt"))
-
-parser = LangchainNodeParser(CharacterTextSplitter())
+lc_splitter = NLTKTextSplitter(chunk_size=160, chunk_overlap=0)
+parser = LangchainNodeParser(lc_splitter)
 nodes = parser.get_nodes_from_documents(document)
-
 for node in nodes:
     print(f"Metadata {node.metadata} \nText: {node.text}")
