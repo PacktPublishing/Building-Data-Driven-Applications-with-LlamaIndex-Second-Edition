9695d463e1bcdc28f5845928c93105efac8b34fa
AG-ITAcademy <andrei.gheorghiu@itacademy.ro>
2026-05-03 13:40:03 +0300

    ch14 updates

--- ch14/ch14_LlamaCloudIndex.py
+++ ch14/ch14_LlamaCloudIndex.py
@@ -1,16 +1,17 @@
 import models_config
 from llama_index.core import SimpleDirectoryReader
-from llama_cloud.lib.index import LlamaCloudIndex
+from llama_cloud_services import LlamaCloudIndex
 
-documents = SimpleDirectoryReader("files").load_data()
+documents = SimpleDirectoryReader("contracts").load_data()
 index = LlamaCloudIndex.from_documents(
     documents,
     "my_first_index",
     project_name="Default",
+    organization_id="<your-organization-id>",
     verbose=True,
 )
 retriever = index.as_retriever()
 chat_engine = index.as_chat_engine()
-response = chat_engine.chat("What types of harm are listed in Figure 1?")
+response = chat_engine.chat("What is the SLA for P3 — Medium incidents?")
 print(response)
 input("Press Enter to exit...")
